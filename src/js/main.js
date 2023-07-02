9e6d3061ede16eefa202a8ba8bf32ea5acad48a6
baibsFar <fararano.kevin@gmail.com>
2023-07-02 15:18:27 +0300

    feat: floating navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
 import { detectOSColorScheme, listenOnColorSchemeChange } from './modules/theme.module.js'
 import { activateMobileMenu, deactivateMobileMenu } from './modules/mobile-navbar.module.js'
-import { BURGER_BUTTON, LANGS, MULTILANG, NAVBAR, NAVBAR_EXIT, ROOT_ELEMENT } from './utils/index.js'
+import { BURGER_BUTTON, FLOATING_LINKS, LANGS, MULTILANG, NAVBAR, NAVBAR_EXIT, ROOT_ELEMENT } from './utils/index.js'
 import { onLangsClick } from './modules/mutli-lang.module.js'
+import { hoverFloatingLink, unhoverFloatingLink } from './modules/floating-navbar.module.js'
 
 function main() {
   try {
@@ -13,6 +14,11 @@ function main() {
     NAVBAR_EXIT.addEventListener('click', () => deactivateMobileMenu(NAVBAR))
 
     MULTILANG.addEventListener('click', () => onLangsClick(LANGS))
+
+    FLOATING_LINKS.forEach(link => {
+      link.addEventListener('mouseenter', () => hoverFloatingLink(link))
+      link.addEventListener('mouseleave', () => unhoverFloatingLink(link))
+    })
   } catch (err) { console.log('CATCHED ERROR --->', err) }
 }
 
